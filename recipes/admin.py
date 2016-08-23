ae31896a6ad73af29a64a2a02545a49e53068a96
nhinsch <nhinsch@gmail.com>
2016-08-23 15:47:20 -0400

    Models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
+
+class RecipeIngredientsInline(admin.StackedInline):
+    model = models.RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientsInline]
 
 # Register your models here.
+admin.site.register(models.Recipe, RecipeAdmin)
+admin.site.register(models.Category)
+admin.site.register(models.CategoryType)
+admin.site.register(models.Ingredient)
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+class CategoryType(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "category types"
+    
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    category_type = models.ForeignKey(CategoryType)
+
+    class Meta:
+        verbose_name_plural = "categories"
+    
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    yields = models.CharField(max_length=255)
+    instructions = models.TextField()
+    serve_with = models.CharField(max_length=255)
+    prep_time = models.DurationField()
+    cooking_time = models.DurationField()
+    ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredient')
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.DecimalField(decimal_places=10, max_digits=10)
+    unit = models.CharField(max_length=255)
+    preparation = models.CharField(max_length=255)
