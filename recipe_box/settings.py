384e75f9e4a6601fe99ea593891ff5b76df49828
nhinsch <nhinsch@gmail.com>
2016-10-04 19:51:14 -0400

    Added logging

--- recipe_box/settings.py
+++ recipe_box/settings.py
@@ -151,3 +151,33 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 #         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
 #     ),
 # }
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'NOTSET',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'NOTSET',
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'propagate': False,
+            'level': 'ERROR'
+        }
+    }
+}
